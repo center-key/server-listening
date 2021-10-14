86688c2b8460fe8ff57e8c0bcccd176604229d7d
dpilafian <dem@centerkey.com>
2021-10-13 22:54:46 -0700

    Update to eslint v8.0

--- dist/server-listening.js
+++ dist/server-listening.js
@@ -1,4 +1,4 @@
-//! server-listening v0.3.2 ~ https://github.com/center-key/server-listening ~ MIT License
+//! server-listening v0.3.2 ~~ https://github.com/center-key/server-listening ~~ MIT License
 
 import cheerio from 'cheerio';
 import express from 'express';
